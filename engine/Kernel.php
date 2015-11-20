ffa17f64f9a5c1111f23202a15ee3a0947626b92
Dwi Hujianto <dwi.hujianto@gmail.com>
2015-11-20 10:23:06 +0700

    Remove Url.php

--- engine/Kernel.php
+++ engine/Kernel.php
@@ -15,7 +15,52 @@ require_once ENGPATH.'/Router.php';
 
 /* Site Url */
 
-require_once ENGPATH.'/Url.php';
+function Segment($segmentvalue)
+{
+	$uri = $_SERVER['REQUEST_URI'];
+
+	$segment = explode('/', $uri);
+
+	switch ($segmentvalue) 
+	{
+		case '2':
+
+			$res = isset($segment[$segmentvalue]) ? $segment[$segmentvalue] : 'base' ;
+
+			break;
+
+		case '3':
+			
+			$res = isset($segment[$segmentvalue]) ? $segment[$segmentvalue] : 'index' ;
+			
+			break;
+		
+		case '4':
+
+			$res = isset($segment[$segmentvalue]) ? $segment[$segmentvalue] : '' ;
+
+			break;
+
+	}
+
+	return $res;
+}
+
+function url($url)
+{
+	$basepath = $_SERVER['HTTP_HOST'];
+
+	return 'http://'.$basepath.'/'.$url;	
+
+}
+
+function redirect($url)
+{
+	$rdr = url($url);
+
+	return header('location:'.$rdr);
+}
+
 
 /* View */
 
